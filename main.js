b6392bd8a22730c9f44eb3c8809fecb9c199712f
Evgeniy Shabolin <steamguardemail@yandex.ru>
2022-11-11 14:27:20 +0300

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.student-groups').slick({
+        slidesToShow: 3,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+})
